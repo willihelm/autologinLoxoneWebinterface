3ba5ba6022dc849fa394dd2a31acc256f6f3141d
Wilhelm Dück <wilhlem.dueck@lampenwelt.de>
2019-02-06 20:09:40 +0100

    initial commit, open loxone webinterface and login

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Builder, By, Key } = require('selenium-webdriver')
+const { Options } = require('selenium-webdriver/chrome')
+
+const chromeOptions = new Options().addArguments('--kiosk', '--disable-infobars')
+
+new Builder()
+  .forBrowser('chrome')
+  .setChromeOptions(chromeOptions)
+  .build()
+  .then((driver) => {
+    driver.getWindowHandle()
+      .then(() => {
+        driver.get(process.env.URL)
+      })
+      .then(() => {
+        return driver.findElement(By.id('username'))
+      })
+      .then((element) => {
+        element.sendKeys(process.env.USERNAME)
+      })
+      .then(() => {
+        return driver.findElement(By.id('password'))
+      })
+      .then((element) => {
+        element.sendKeys(process.env.PASSWORD, Key.RETURN)
+      })
+      .then(() => {
+        // driver.manage().window().maximize()
+      })
+  })
